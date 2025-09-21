2b3da34f858c4fa1ff601071fe21d32f93210666
maub-dev <mau.boff@gmail.com>
2025-09-21 09:57:38 -0300

    Unit Tests improvement - validate exception message

--- ABInBev.Employees/ABInBev.Employees.Business.Tests/EmployeeServiceTests.cs
+++ ABInBev.Employees/ABInBev.Employees.Business.Tests/EmployeeServiceTests.cs
@@ -4,6 +4,7 @@ using ABInBev.Employees.Business.Models.Enums;
 using ABInBev.Employees.Business.Services;
 using Microsoft.AspNetCore.Identity;
 using Moq;
+using System.Runtime.Intrinsics.X86;
 
 namespace ABInBev.Employees.Business.Tests
 {
@@ -70,12 +71,14 @@ namespace ABInBev.Employees.Business.Tests
                 .Setup(x => x.GetByEmailAsync(It.IsAny<string>()))
                 .ReturnsAsync(_validEmployee);
 
+            const string errorMessage = "Invalid Password";
             _userManagerMock
                 .Setup(x => x.CreateAsync(It.IsAny<IdentityUser>(), It.IsAny<string>()))
-                .ReturnsAsync(IdentityResult.Failed(new IdentityError { Code = "Error", Description = "Invalid Password" }));
+                .ReturnsAsync(IdentityResult.Failed(new IdentityError { Code = "Error", Description = errorMessage }));
 
             // Act & Assert
-            await Assert.ThrowsAsync<InvalidOperationException>(() => _service.AddAsync(_validEmployee, "pass", string.Empty));
+            var exception = await Assert.ThrowsAsync<InvalidOperationException>(() => _service.AddAsync(_validEmployee, "pass", string.Empty));
+            Assert.Contains(errorMessage, exception.Message);
         }
 
         [Fact]
@@ -96,7 +99,8 @@ namespace ABInBev.Employees.Business.Tests
                 .Returns(Task.CompletedTask);
 
             // Act & Assert
-            await Assert.ThrowsAsync<InvalidOperationException>(() => _service.AddAsync(_validEmployee, "Pa$$word1!", string.Empty));
+            var exception = await Assert.ThrowsAsync<InvalidOperationException>(() => _service.AddAsync(_validEmployee, "Pa$$word1!", string.Empty));
+            Assert.Contains("is not a valid email address", exception.Message);
         }
 
         [Fact]
@@ -120,7 +124,8 @@ namespace ABInBev.Employees.Business.Tests
                 .ReturnsAsync(() => true);
 
             // Act & Assert
-            await Assert.ThrowsAsync<InvalidOperationException>(() => _service.AddAsync(_validEmployee, "Pa$$word1!", string.Empty));
+            var exception = await Assert.ThrowsAsync<InvalidOperationException>(() => _service.AddAsync(_validEmployee, "Pa$$word1!", string.Empty));
+            Assert.Contains("This Document Number is already in use", exception.Message);
         }
 
         [Fact]
@@ -148,7 +153,8 @@ namespace ABInBev.Employees.Business.Tests
                 .ReturnsAsync(() => true);
 
             // Act & Assert
-            await Assert.ThrowsAsync<InvalidOperationException>(() => _service.AddAsync(_validEmployee, "Pa$$word1!", string.Empty));
+            var exception = await Assert.ThrowsAsync<InvalidOperationException>(() => _service.AddAsync(_validEmployee, "Pa$$word1!", string.Empty));
+            Assert.Contains("This Email is already in use", exception.Message);
         }
 
         [Fact]
@@ -169,7 +175,8 @@ namespace ABInBev.Employees.Business.Tests
                 .Returns(Task.CompletedTask);
 
             // Act & Assert
-            await Assert.ThrowsAsync<InvalidOperationException>(() => _service.AddAsync(_validEmployee, "Pa$$word1!", string.Empty));
+            var exception = await Assert.ThrowsAsync<InvalidOperationException>(() => _service.AddAsync(_validEmployee, "Pa$$word1!", string.Empty));
+            Assert.Contains("'Phone1' must not be empty", exception.Message);
         }
 
         [Fact]
@@ -190,7 +197,8 @@ namespace ABInBev.Employees.Business.Tests
                 .Returns(Task.CompletedTask);
 
             // Act & Assert
-            await Assert.ThrowsAsync<InvalidOperationException>(() => _service.AddAsync(_validEmployee, "Pa$$word1!", string.Empty));
+            var exception = await Assert.ThrowsAsync<InvalidOperationException>(() => _service.AddAsync(_validEmployee, "Pa$$word1!", string.Empty));
+            Assert.Contains("'Phone2' must not be empty", exception.Message);
         }
 
         [Fact]
@@ -211,7 +219,8 @@ namespace ABInBev.Employees.Business.Tests
                 .Returns(Task.CompletedTask);
 
             // Act & Assert
-            await Assert.ThrowsAsync<InvalidOperationException>(() => _service.AddAsync(_validEmployee, "Pa$$word1!", string.Empty));
+            var exception = await Assert.ThrowsAsync<InvalidOperationException>(() => _service.AddAsync(_validEmployee, "Pa$$word1!", string.Empty));
+            Assert.Contains("'Phone2' must not be equal to", exception.Message);
         }
 
         [Fact]
@@ -232,11 +241,12 @@ namespace ABInBev.Employees.Business.Tests
                 .Returns(Task.CompletedTask);
 
             // Act & Assert
-            await Assert.ThrowsAsync<InvalidOperationException>(() => _service.AddAsync(_validEmployee, "Pa$$word1!", string.Empty));
+            var exception = await Assert.ThrowsAsync<InvalidOperationException>(() => _service.AddAsync(_validEmployee, "Pa$$word1!", string.Empty));
+            Assert.Contains("The Employee must have at least 18 years old", exception.Message);
         }
 
         [Fact]
-        public async Task UpdateAsync_ValidEmployee_UpdateEmployeeAndPhonebook()
+        public async Task UpdateAsync_ValidEmployee_UpdateEmployee()
         {
             // Arrange
             _employeeRepoMock.Setup(x => x.GetByIdAsync(It.IsAny<Guid>())).ReturnsAsync(_validEmployee);
@@ -264,7 +274,8 @@ namespace ABInBev.Employees.Business.Tests
                 .ReturnsAsync(_validEmployee);
 
             // Act & Assert
-            await Assert.ThrowsAsync<InvalidOperationException>(() => _service.UpdateAsync(_validEmployee, string.Empty));
+            var exception = await Assert.ThrowsAsync<InvalidOperationException>(() => _service.UpdateAsync(_validEmployee, string.Empty));
+            Assert.Contains($"The Employee {_validEmployee.Id} was not found.", exception.Message);
         }
 
         [Fact]
